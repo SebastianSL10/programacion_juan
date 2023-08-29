490782479452d115df28ef3a7b9c61d662aff0ba
SebastianSL10 <119981305+SebastianSL10@users.noreply.github.com>
2023-08-28 19:28:32 -0500

    Carpeta con las clases Álbum, Cancion, Usuario

---
+++ modelos/modelos.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+import enum
+
+db = SQLAlchemy()
+
+class Cancion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    minutos = db.Column(db.Integer)
+    segundos = db.Column(db.Integer)
+    interprete = db.Column(db.String(128))
+
+class Medio (enum.Enum):
+    DISCO = 1
+    Cassette = 2
+    CD = 3
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    anio = db.Column(db.Integer)
+    description = db.Column(db.String(512))
+    medio = db.Column(db.Enum(Medio))
+    usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    __table_args__ = (db.UniqueConstraint('usuario', 'titulo',name='titulo_unico_album'))
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50))
+    contrasena = db.Column(db.String(50))
+    albumes = db.relationship('Album', cascade='all, delete, delete-orphan')
+
