3732918dbd0da43f3af0b5616c34a946122ff228
SebastianSL10 <119981305+SebastianSL10@users.noreply.github.com>
2023-08-29 18:30:58 -0500

    Crud de la tabla cancion
    :)

---
+++ flaskr/app.py
@@ -0,0 +1,60 @@
+from flaskr import create_app
+from .modelos import db, Cancion
+from flask_restful import Api
+from .vista import vista_canciones, vista_cancion
+
+app = create_app("default")
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+#inicio la api
+api = Api(app)
+
+#ruta a la vista que quiero consultar parametro la clase a usar y la ruta esta clase tiene tanto get como post dentro
+api.add_resource(vista_canciones,'/canciones')
+api.add_resource(vista_cancion,'/canciones/<int:id>')
+
+
+'''from flaskr import create_app
+from .modelos import db, Cancion, Usuario, Album, Medio
+from .modelos import AlbumSchema
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+with app.app_context():
+    Album_Schema = AlbumSchema()
+    A = Album(titulo='Prueba', anio=2022, description='texto', medio=Medio.CD)
+    db.session.add(A)
+    db.session.commit()
+    print([Album_Schema.dumps(album) for album in Album.query.all()])
+#prueba
+with app.app_context():
+    c = Cancion(titulo='Prueba', minutos=2, segundos=25, interprete='Carlos')
+    db.session.add(c)
+    db.session.commit()
+    print(Cancion.query.all())
+with app.app_context():
+    c = Cancion(titulo='si', minutos=3, segundos=20, interprete='Arcangel')
+    u = Usuario(nombre='sebastian', contrasena='password')
+    a = Album(titulo='prueba', anio=2000, description='Maravilla', medio=Medio.CD)
+    a.canciones.append(c)
+    u.albumes.append(a)
+    db.session.add(a)
+    db.session.add(u)
+    db.session.commit()
+    print(Usuario.query.all())
+    print(Usuario.query.all()[0].albumes)
+    db.session.delete(u)
+    print(Usuario.query.all())
+    print(Album.query.all())
+    print(Cancion.query.all())
+'''
+
